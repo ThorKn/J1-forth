c24070261991bea50c11ad6379539eee4666bb33
Thorsten Knoll <thorsten.b.knoll@student.hs-rm.de>
2017-03-10 10:30:21 +0100

    added new project 'oledrgb' for the J1A CPU

---
+++ oledrgb/README.md
@@ -0,0 +1,17 @@
+This is the start of a library and maybe a little game for the J1 CPU and the Digilent Pmod OLEDRGB_96x64. The Pmod is driven by the SPI-Protocol.
+So far it works with The J1A CPU from James Bowmans swapforth-implementation on an Lattice IceStick. The OLEDRGB-Pmod is attached to the IceStick via the Pmod-Connector with the standard pinlayout.
+
+Get the OLEDRGB_96x64 to work:
+
+1. Boot up the J1A on an IceStick
+2. Get a connection to the shell.py on your PC
+3. #include oledrgb.fs
+5. endless
+
+Notes:
+
+- This project is purely for fun.
+- It's work-in-progress and not nearly finished.
+- The SPI Implementation is my first try on this protocol and is surely crappy. But hey... It works somehow!
+
+
---
+++ oledrgb/oledrgb.fs
@@ -0,0 +1,177 @@
+\ ---------------------------------------------
+\ -- Module: OLEDRGB in ANS-FORTH -------------
+\ -- Author: Thorsten Knoll -------------------
+\ -- Date:   March 2017 -----------------------
+\ -- Written for J1A CPU ----------------------
+\ ---------------------------------------------
+
+#include oledrgb_spi.fs
+
+: OLED-INIT
+
+  \ --- Pmod direction to out
+  FF $0002 io!
+
+  \ --- Pin-state to zero
+  0 PIN-STATE !
+  PIN-STATE @ PINS!
+
+  \ --- ChipSelect high
+  CS-PIN HIGH  
+
+  \ --- DC low
+  DC-PIN LOW
+
+  \ --- Reset high
+  RES-PIN HIGH
+
+  \ --- VccEn low
+  VCCEN-PIN LOW
+
+  \ --- PomEn high
+  PMODEN-PIN HIGH
+  20 MS
+
+  \ --- Do reset
+  RES-PIN LOW
+  2 MS
+  RES-PIN HIGH
+  2 MS
+
+  SCK-PIN HIGH
+  1 MS
+
+  \ --- unlock command
+  FD SPI-SEND
+  12 SPI-SEND
+
+  \ --- display off command
+  AE SPI-SEND
+
+  \ --- remap and display format
+  A0 SPI-SEND
+  72 SPI-SEND
+
+  \ --- startline to topline
+  A1 SPI-SEND
+  00 SPI-SEND
+ 
+  \ --- no vertical offset
+  A2 SPI-SEND
+  00 SPI-SEND
+
+  \ --- no color inversion
+  A4 SPI-SEND
+
+  \ --- multiplex ratio: all common pins enabled
+  A8 SPI-SEND
+  3F SPI-SEND
+
+  \ --- external Vcc enabled
+  AD SPI-SEND
+  8E SPI-SEND
+
+  \ --- disable power-saving mode
+  B0 SPI-SEND
+  0B SPI-SEND
+
+  \ --- set the phase length
+  B1 SPI-SEND
+  31 SPI-SEND
+
+  \ --- set clock divide ratio and osci frequency
+  B3 SPI-SEND
+  F0 SPI-SEND
+
+  \ --- pre-charge speed of color A
+  8A SPI-SEND
+  64 SPI-SEND
+
+  \ --- pre-charge speed of color B
+  8B SPI-SEND
+  78 SPI-SEND
+
+  \ --- pre-charge speed of color C
+  8C SPI-SEND
+  64 SPI-SEND
+
+  \ --- pre-charge voltage to 45% of Vcc
+  BB SPI-SEND
+  3A SPI-SEND
+
+  \ --- VCOMH Deselct level
+  BE SPI-SEND
+  3E SPI-SEND
+
+  \ --- Master current attentuation factor
+  87 SPI-SEND
+  06 SPI-SEND
+
+  \ --- Contrast for color A
+  81 SPI-SEND
+  91 SPI-SEND
+
+  \ --- Contrast for color B
+  82 SPI-SEND
+  50 SPI-SEND
+
+  \ --- Contrast for color C
+  83 SPI-SEND
+  7D SPI-SEND
+
+  \ --- disable scrolling
+  25 SPI-SEND
+
+  \ --- clear screen and window dimensions to clear
+  25 SPI-SEND
+  00 SPI-SEND
+  00 SPI-SEND
+  5F SPI-SEND
+  3F SPI-SEND
+
+  \ --- VCCEN high + 25ms wait
+  VCCEN-PIN HIGH
+  25 MS
+
+  \ --- display on
+  AF SPI-SEND
+  100 MS ;
+
+: OLED-OFF
+  AE SPI-SEND
+  VCCEN-PIN LOW
+  400 MS ;
+
+: CLEAR
+  3F 5F 0 0 25
+  5 0 DO SPI-SEND LOOP ;
+
+: LINE ( y2, x2, y1, x1 --- )
+  21 SPI-SEND
+  4 0 DO SPI-SEND LOOP
+  35 35 35
+  3 0 DO SPI-SEND LOOP ;
+
+: LINE-MOVE
+  5F 0 DO
+    CLEAR
+    3F I 0 5F I - LINE
+    100 US
+  LOOP
+  0 5F DO
+    CLEAR
+    3F I 0 5F I - LINE
+    100 US
+  -1 +LOOP
+  ;
+
+: ENDLESS
+  BEGIN
+    LINE-MOVE
+  AGAIN
+  ;
+
+
+
+
+
